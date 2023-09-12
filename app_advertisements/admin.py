3ed5b5b7e531896fc1213387fe2c7e5976dbba44
ebqqq <denis.litvincev02@gmail.com>
2023-09-12 20:42:19 +0800

    first commit

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+class AdvetisementAdmin(admin.ModelAdmin):
+    list_display = ['id','title','description', 'price', 'auction', 'created_date','updated_date']
+    list_filter = ['auction','created_at']
+
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+
+    @admin.action(description = 'Убрaть возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction = False)
+
+    @admin.action(description = 'Добавить возможность торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction = True)
+
+
+
+    fieldsets = (
+        ('Общее', {
+            'fields' : ('title' ,'description'),
+        }),
+        ('Финанасы' , {
+            'fields' : ('price', 'auction'),
+            'classes' : ['collapse']
+        }),
+    )
+
+
+
+
+admin.site.register(Advertisement, AdvetisementAdmin)
+
+# Register your models here. Добавить возможность торга
